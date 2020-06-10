54f840a36489c823829bc17b90db81bb8690ec77
”ayaka-8” <grape8ayk208@yahoo.co.jp>
2020-06-10 01:58:51 +0000

    ナビゲーションバーとログイン機能の追加

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+@section('title', 'プロフィール登録')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール登録</h2>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@update');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
