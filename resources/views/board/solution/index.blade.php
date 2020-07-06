91c939ceba4839dc5bb2a8a633db77b8672ab83d
EC2 Default User <ec2-user@ip-172-31-29-85.us-east-2.compute.internal>
2020-07-06 05:31:38 +0000

    responsive test9

--- resources/views/board/solution/index.blade.php
+++ resources/views/board/solution/index.blade.php
@@ -26,8 +26,8 @@
                         <h4>ソリューション一覧</h4>
                         @if(count($solution_boards) > 0)
                         @foreach($solution_boards as $board)
-                        <div class="media d-flex flex-sm-column flex-sm-wrap flex-md-row">
-                            <div class="media-image flex-md-fill my-3">
+                        <div class="media">
+                            <div class="media-image col my-3">
                                 @if($board->solution_image != null)
                                 <!--ソリューションに関する画像の表示-->
                                 <img src="{{ $board->solution_image }}" class="img-fluid thumbnail mx-3" alt="ソリューションに関する画像の表示" data-toggle="modal" data-target="#image-modal" style="cursor:pointer">
@@ -44,7 +44,7 @@
                                 @endif
                             </div>
                             
-                            <div class="media-body px-2 flex-md-fill my-3">
+                            <div class="media-body col px-2 my-3">
                                 <h5 class="mt-0">ソリューションキーワード</h5>
                                 <p>{!! nl2br($board->solution_keyword) !!}</p>
                                 <h5 class="mt-0">ソリューション会社名</h5>
