2c76f16d8fb07df631744ca1888bc755ff190262
EC2 Default User <ec2-user@ip-172-31-29-85.us-east-2.compute.internal>
2020-07-06 06:05:41 +0000

    responsive test11

--- resources/views/board/solution/index.blade.php
+++ resources/views/board/solution/index.blade.php
@@ -26,7 +26,7 @@
                         <h4>ソリューション一覧</h4>
                         @if(count($solution_boards) > 0)
                         @foreach($solution_boards as $board)
-                        <div class="media">
+                        <div class="media row">
                             <div class="media-image col my-3">
                                 @if($board->solution_image != null)
                                 <!--ソリューションに関する画像の表示-->
