ffe715551d10e377e528645f1dcfc9540b6e66e7
EC2 Default User <ec2-user@ip-172-31-29-85.us-east-2.compute.internal>
2020-08-02 07:26:27 +0000

    ホーム画面レイアウト変更5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,21 +25,21 @@
                     <!--新着ソリューション情報-->
                     <div class="solution-info card my-3 border-white">
                         <div class="card-body">
-                            <h5 class="col-12 card-header px-3 text-light bg-dark">新着ソリューション情報</h5>
+                            <h5 class="col-12 col card-header px-3 text-light bg-dark">新着ソリューション情報</h5>
                             @if (count($solutions) > 0)
                             @foreach ($solutions as $solution)
-                            <div class="solution-group col-12 card-body px-1 py-2">
+                            <div class="solution-group col-12 col card-body px-1 py-2">
                                  <div class="updated_solutions-body row no-gutters flex-column flex-sm-column flex-md-column justify-md-content-around">
-                                    　<a class="updated_solutions-body-item col-12 col-sm-12 col-md-12 pt-2 pb-2 pb-sm-1 pb-md-2 px-1 px-sm-4 px-md-0 text-decoration-none" href="{{ action('SolutionBoardController@show', ['id' => $solution->id]) }}">
-                                    　     <div class="d-flex flex-row flex-sm-row align-items-center">
-                                    　          <div class="col-4 col-sm-4 col-md-4 px-1 px-sm-0 px-md-1 pt-0 pt-sm-2 pt-md-0">
+                                    　<a class="updated_solutions-body-item col-12 col-sm-12 col-md-12 col pt-2 pb-2 pb-sm-1 pb-md-2 px-1 px-sm-4 px-md-0 text-decoration-none" href="{{ action('SolutionBoardController@show', ['id' => $solution->id]) }}">
+                                    　     <div class="d-flex flex-row flex-sm-row flex-column align-items-center">
+                                    　          <div class="col-4 col-sm-4 col-md-4 col px-1 px-sm-0 px-md-1 pt-0 pt-sm-2 pt-md-0">
                                     　           　@if($solution->solution_image != null)
-                                    　           　 <img width="120" height="100" layout="responsive" class="updated_solutions-body-item-img img-fluid _w-100 text-center" src="{{ $solution->solution_image }}">
+                                    　             <img width="120" height="100" layout="responsive" class="updated_solutions-body-item-img img-fluid _w-100 text-center" src="{{ $solution->solution_image }}">
                                     　           　@else
                                     　             <img width="120" height="100" layout="responsive" src="{{ $no_image }}" class="img-fluid _w-100 text-center mx-3">
                                     　             @endif
                                     　       　 </div>
-                                    　       　 <div class="col-8 col-sm-8 col-md-8 px-1 px-sm-0 px-md-1">
+                                    　       　 <div class="col-8 col-sm-8 col-md-8 col px-1 px-sm-0 px-md-1">
                                     　       　　　    <div class="update_solutions-item-name text-left text-dark" >
                                     　           　　　    {{ $solution->public_name }}
                                     　           　 </div>
@@ -64,21 +64,21 @@
                     <!--新着お悩み情報-->
                     <div class="challenge-info card my-3 border-white">
                         <div class="card-body">
-                            <h5 class="col-12 card-header px-3 text-light bg-dark">新着お悩み情報</h5>
+                            <h5 class="col-12 col card-header px-3 text-light bg-dark">新着お悩み情報</h5>
                             @if (count($challenges) > 0)
                             @foreach ($challenges as $challenge)
                             <div class="challenge-group col-12 card-body px-1 py-2">
                                 <div class="updated-challenges-body row no-gutters flex-column flex-sm-column flex-md-column justify-md-content-around">
-                                    <a class="updated_challenges-body-item col-12 col-sm-12 col-md-12 pt-2 pb-2 pb-sm-1 pb-md-2 px-1 px-sm-4 px-md-0 text-decoration-none" href="{{ action('ChallengeBoardController@show', ['id' => $challenge->id]) }}">
-                                        <div class="d-flex flex-row flex-sm-row align-items-center">
-                                            <div class="col-4 col-sm-4 col-md-4 px-1 px-sm-0 px-md-1 pt-0 pt-sm-2 pt-md-0">
+                                    <a class="updated_challenges-body-item col-12 col-sm-12 col-md-12 col pt-2 pb-2 pb-sm-1 pb-md-2 px-1 px-sm-4 px-md-0 text-decoration-none" href="{{ action('ChallengeBoardController@show', ['id' => $challenge->id]) }}">
+                                        <div class="d-flex flex-row flex-sm-row flex-column align-items-center">
+                                             <div class="col-4 col-sm-4 col-md-4 col px-1 px-sm-0 px-md-1 pt-0 pt-sm-2 pt-md-0">
                                                 @if ($challenge->challenge_image != null)
                                     　           <img width="120" height="100" layout="responsive" class="updated_challenges-body-item-img img-fluid _w-100 text-center" src="{{ $challenge->challenge_image }}">
                                     　           @else
                                     　           <img width="120" height="100" layout="responsive" src="{{ $no_image }}" class="img-fluid _w-100 text-center mx-3">
                                     　          @endif
                                     　       </div>
-                                    　       <div class="col-8 col-sm-8 col-md-8 px-1 px-sm-0 px-md-1">
+                                    　       <div class="col-8 col-sm-8 col-md-8 col px-1 px-sm-0 px-md-1">
                                     　           <div class="update_solutions-item-name text-left text-dark">
                                     　               {{ $challenge->public_name }}
                                     　           </div>
