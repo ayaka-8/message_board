09bd3a940c4ccf4d62ff005815f4eaccf9157c91
EC2 Default User <ec2-user@ip-172-31-29-85.us-east-2.compute.internal>
2020-08-15 08:50:24 +0000

    ホーム画面レイアウト変更7

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,28 +23,28 @@
             <div class="col-md-10 mx-auto">
                 <div class="latest-info col-md-10 mx-auto">
                     <!--新着ソリューション情報-->
-                    <div class="solution-info card col my-3 border-white">
+                    <div class="solution-info card col-md-10 mx-auto my-3 border-white">
                         <div class="card-body col">
                             <h5 class="col-12 col card-header px-3 text-light bg-dark">新着ソリューション情報</h5>
                             @if (count($solutions) > 0)
                             @foreach ($solutions as $solution)
                             <div class="solution-group col-12 col card-body px-1 py-2">
                                  <div class="updated_solutions-body row no-gutters flex-column flex-sm-column flex-md-column justify-md-content-around">
-                                    　<a class="updated_solutions-body-item col-12 col-sm-12 col-md-12 col pt-2 pb-2 pb-sm-1 pb-md-2 px-1 px-sm-4 px-md-0 text-decoration-none" href="{{ action('SolutionBoardController@show', ['id' => $solution->id]) }}">
+                                    　<a class="updated_solutions-body-item col-12 col-sm-12 col-md-12 col pt-2 pb-2 pb-sm-1 pb-md-2 px-1 px-sm-4 px-md-0 text-decoration-none text-center" href="{{ action('SolutionBoardController@show', ['id' => $solution->id]) }}">
                                     　     <div class="d-flex flex-row flex-sm-row flex-column align-items-center">
-                                    　          <div class="col-4 col-sm-4 col-md-4 col px-1 px-sm-0 px-md-1 pt-0 pt-sm-2 pt-md-0">
+                                    　          <div class="col col-sm-4 col-md-4 col px-1 px-sm-0 px-md-1 pt-0 pt-sm-2 pt-md-0">
                                     　           　@if($solution->solution_image != null)
                                     　             <img width="120" height="100" layout="responsive" class="updated_solutions-body-item-img img-fluid _w-100 text-center" src="{{ $solution->solution_image }}">
                                     　           　@else
                                     　             <img width="120" height="100" layout="responsive" src="{{ $no_image }}" class="img-fluid _w-100 text-center mx-3">
                                     　             @endif
                                     　       　 </div>
-                                    　       　 <div class="col-8 col-sm-8 col-md-8 col px-1 px-sm-0 px-md-1">
+                                    　       　 <div class="col col-sm-8 col-md-8 col px-1 px-sm-0 px-md-1">
                                     　       　　　    <div class="update_solutions-item-name text-left text-dark" >
                                     　           　　　    {{ $solution->public_name }}
                                     　           　 </div>
                                     　       　　　    <div class="updated_solutions-item-desc text-left text-dark col">
-                                    　               　 {{ $solution->solution_keyword }}
+                                    　               　ソリューションキーワード： {{ $solution->solution_keyword }}
                                     　           　 </div>
                                     　       　　　    <div class="updated-solutions-item-date text-right text-dark">
                                     　           　　    {{ $solution->created_at->format('Y/m/d') }}
@@ -62,7 +62,7 @@
                         </div>
                     </div>
                     <!--新着お悩み情報-->
-                    <div class="challenge-info card col my-3 border-white">
+                    <div class="challenge-info card col-md-10 mx-auto my-3 border-white">
                         <div class="card-body col">
                             <h5 class="col-12 col card-header px-3 text-light bg-dark">新着お悩み情報</h5>
                             @if (count($challenges) > 0)
@@ -83,7 +83,7 @@
                                     　               {{ $challenge->public_name }}
                                     　           </div>
                                     　           <div class="updated_challenges-item-desc text-left text-dark col">
-                                    　               {{ $challenge->challenge_keyword }}
+                                    　              お悩みキーワード： {{ $challenge->challenge_keyword }}
                                     　           </div>
                                     　           <div class="updated_challenges-item-desc text-left text-dark">
                                     　               {{ $challenge->created_at->format('Y/m/d') }}
